fdfacea4dd2277969a37c9371e26fd6352bf3d15
piotrmikula <p.mikula14@wp.pl>
2020-12-03 15:10:58 +0000

    Zadanie12

--- lab5.cpp
+++ lab5.cpp
@@ -1,29 +1,56 @@
 #include "Human.hpp"
 #include <iostream>
-#include <list>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    list< int > lista;
-    lista.push_back(14);
-    lista.push_front(3);
-    cout << "Lista:\n";
-    cout << "Pierwszy element: " << lista.front() << ", Ostatni element: " << lista.back() << endl;
-    cout << "Adresy:\n";
-    cout << "Pierwszy element: " << &lista.front() << ", Ostatni element: " << &lista.back()
-         << endl;
+    Human h1, h2, h3;
 
-    cout << "\nDodanie kilku elementow na poczatek listy\n";
-    lista.push_front(1);
-    lista.push_front(5);
-    lista.push_front(10);
-    cout << "Adresy:\n";
-    cout << "Pierwszy element: " << &lista.front() << ", Ostatni element: " << &lista.back()
-         << endl;
+    vector< Human > people(3);
+    string          imie;
+    unsigned int    wiek;
+    bool            ld;
+    bool            lk;
 
-    // cout << lista[2] << endl;
-    //^ po odkomentowaniu wyskakuje error
+    vector< Human >::iterator it = people.begin();
+    // vector< Human >::iterator end = people.end();
+
+    for (it = people.begin(); it != people.end(); ++it) {
+        cout << "Podaj imie:\t";
+        cin >> imie;
+        if (imie == "skasuj") {
+            people.pop_back();
+            it--;
+            cout << "Podaj imie:\t";
+            cin >> imie;
+        }
+        cout << "Podaj wiek:\t";
+        cin >> wiek;
+        cout << "Czy lubi pjeski (1 - tak, 0 - nie)\t";
+        cin >> ld;
+        cout << "Czy lubi koty (1 - tak, 0 - nie)\t";
+        cin >> lk;
+        cout << "\n";
+
+        Human temp{imie, wiek, ld, lk};
+        *it = temp;
+    }
+
+    cout << "Wektor\n";
+    cout << "rozmiar: " << people.size() << ", pojemnosc: " << people.capacity() << endl;
+
+    cout << "\nMOI LUDZIE TO\n";
+    for (it = people.begin(); it != people.end(); ++it) {
+        Human temp{};
+        temp = *it;
+        cout << temp.getName() << ", " << temp.getAge() << " lat, ";
+        if (temp.isMonster() == 1)
+            cout << "POTWOR\n";
+        if (temp.isMonster() == 0)
+            cout << "KOKS\n";
+    }
 
     puts("\nOstatnia linijka kodu!");
 }
