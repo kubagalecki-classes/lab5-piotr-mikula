bf739f51226634d5c2562c36cb4fed3459047802
piotrmikula <p.mikula14@wp.pl>
2020-12-03 13:42:08 +0000

    Zadanie9

--- lab5.cpp
+++ lab5.cpp
@@ -8,7 +8,11 @@ int main()
     list< int > lista;
     lista.push_back(14);
     lista.push_front(3);
+    cout << "Lista:\n";
     cout << "Pierwszy element: " << lista.front() << ", Ostatni element: " << lista.back() << endl;
+    cout << "Adresy:\n";
+    cout << "Pierwszy element: " << &lista.front() << ", Ostatni element: " << &lista.back()
+         << endl;
 
     puts("\nOstatnia linijka kodu!");
 }
