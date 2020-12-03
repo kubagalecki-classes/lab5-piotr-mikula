a5db32abf445caf0a18257b5625cb91206a66d4e
piotrmikula <p.mikula14@wp.pl>
2020-12-03 15:12:48 +0000

    Zadanie13

--- lab5.cpp
+++ lab5.cpp
@@ -1,56 +1,23 @@
 #include "Human.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
-    Human h1, h2, h3;
+    list< int > lista;
+    lista.push_back(14);
+    lista.push_front(3);
+    lista.push_front(1);
+    lista.push_front(5);
+    lista.push_front(10);
 
-    vector< Human > people(3);
-    string          imie;
-    unsigned int    wiek;
-    bool            ld;
-    bool            lk;
+    cout << "Lista:\n";
+    cout << "Pierwszy element: " << lista.front() << ", Ostatni element: " << lista.back() << endl;
 
-    vector< Human >::iterator it = people.begin();
-    // vector< Human >::iterator end = people.end();
-
-    for (it = people.begin(); it != people.end(); ++it) {
-        cout << "Podaj imie:\t";
-        cin >> imie;
-        if (imie == "skasuj") {
-            people.pop_back();
-            it--;
-            cout << "Podaj imie:\t";
-            cin >> imie;
-        }
-        cout << "Podaj wiek:\t";
-        cin >> wiek;
-        cout << "Czy lubi pjeski (1 - tak, 0 - nie)\t";
-        cin >> ld;
-        cout << "Czy lubi koty (1 - tak, 0 - nie)\t";
-        cin >> lk;
-        cout << "\n";
-
-        Human temp{imie, wiek, ld, lk};
-        *it = temp;
-    }
-
-    cout << "Wektor\n";
-    cout << "rozmiar: " << people.size() << ", pojemnosc: " << people.capacity() << endl;
-
-    cout << "\nMOI LUDZIE TO\n";
-    for (it = people.begin(); it != people.end(); ++it) {
-        Human temp{};
-        temp = *it;
-        cout << temp.getName() << ", " << temp.getAge() << " lat, ";
-        if (temp.isMonster() == 1)
-            cout << "POTWOR\n";
-        if (temp.isMonster() == 0)
-            cout << "KOKS\n";
-    }
+    cout << "\nDrukowanie elementow od pierwszego do ostatniego\n";
+    for (list< int >::iterator it = lista.begin(); it != lista.end(); ++it)
+        cout << *it << endl;
 
     puts("\nOstatnia linijka kodu!");
 }
