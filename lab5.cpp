1235117b3e922096b8f60a61f5500a671c711e59
piotrmikula <p.mikula14@wp.pl>
2020-12-03 12:13:24 +0000

    Zadanie3

--- lab5.cpp
+++ lab5.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 int main()
 {
-    vector< int > v;
+    vector< int > v(100, 42);
     cout << "Rozmiar wektora v = " << v.size() << endl;
     cout << "Pojemnosc wektora v = " << v.capacity() << endl;
+    for (int i = 0; i < 15; i++)
+        cout << v[i] << endl;
+    cout << "\n";
 
-    cout << "\nDodawanie elementow do wektora\n";
-    for (int i = 0; i < 10; i++) {
-        v.push_back(2 * i);
-        cout << "Rozmiar: " << v.size() << " Pojemnosc: " << v.capacity() << endl;
-        cout << "Adres pierwszego elementu: " << &v[0] << endl;
-    }
+    vector< int > x{100, 42};
+    cout << "Rozmiar wektora x = " << v.size() << endl;
+    cout << "Pojemnosc wektora x = " << v.capacity() << endl;
+    for (int i = 0; i < 15; i++)
+        cout << x[i] << endl;
 
     puts("\nOstatnia linijka kodu!");
 }
