aa82afac2c5b484736f69d5bf806e4c0af3ae611
piotrmikula <p.mikula14@wp.pl>
2020-12-03 13:38:25 +0000

    Zadanie7

--- lab5.cpp
+++ lab5.cpp
@@ -1,48 +1,13 @@
 #include "Human.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
-    vector< Human > people;
-    string          imie;
-    unsigned int    wiek;
-    bool            ld;
-    bool            lk;
-    for (int i = 0; i < 3; i++) {
-        cout << "Podaj imie:\t";
-        cin >> imie;
-        if (imie == "skasuj") {
-            people.pop_back();
-            i--;
-            cout << "Podaj imie:\t";
-            cin >> imie;
-        }
-        cout << "Podaj wiek:\t";
-        cin >> wiek;
-        cout << "Czy lubi pjeski (1 - tak, 0 - nie)\t";
-        cin >> ld;
-        cout << "Czy lubi koty (1 - tak, 0 - nie)\t";
-        cin >> lk;
-        cout << "\n";
-
-        Human temp{imie, wiek, ld, lk};
-        people.push_back(temp);
-    }
-
-    cout << "Wektor\n";
-    cout << "rozmiar: " << people.size() << ", pojemnosc: " << people.capacity() << endl;
-
-    cout << "\nMOI LUDZIE TO\n";
-    for (unsigned int i = 0; i < people.size(); i++) {
-        cout << people[i].getName() << ", " << people[i].getAge() << " lat, ";
-        if (people[i].isMonster() == 1)
-            cout << "POTWOR\n";
-        if (people[i].isMonster() == 0)
-            cout << "KOKS\n";
-    }
+    list< int > lista;
+    lista.push_back(14);
+    cout << "Pierwszy element: " << lista.front() << " Ostatni element: " << lista.back() << endl;
 
     puts("\nOstatnia linijka kodu!");
 }
